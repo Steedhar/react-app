500c517d0442521fbd9b1a3c2bea0dde57d94ca9
steedhar <steedharravi@gmail.com>
2025-09-29 21:57:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello from React + Nginx + Docker + GitHub Actions + AWS EC2 🚀</h1>
+      <p>This is a sample CI/CD deployed React app.</p>
+    </div>
+  );
+}
+
+export default App;
