972fe31024bf8cceaea5b7947a3d3e9f179811be
steedhar <steedharravi@gmail.com>
2025-10-02 21:15:51 +0530

    smalhhl

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="app">
       <h1>Hello from React + Nginx + Docker + GitHub Actions + AWS EC2 🚀</h1>
       <p>This is a sample CI/CD deployed React app.</p>
-      <p>This is a mahesh.</p>
+      <p>This is a mahesh. hello</p>
     </div>
   );
 }
